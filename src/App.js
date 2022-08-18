128875df5e913691d1b032200b77753163689f35
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-18 14:25:01 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Weather from './Weather';
 function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
-      <Weather />
-      <footer>
-        This project was created by <a href='https://dapper-stardust-7d2615.netlify.app/index.html' target="_blank" rel="noreferrer">Beatriz Barbosa</a> and is available on
-        <a href='https://github.com/beatrizmcbarbosa/weather-react' target="_blank" rel="noreferrer">GitHub</a>
-      </footer>
+      <div className="container">
+        <h2>Weather App</h2>
+        <Weather />
+        <footer>
+          This project was created by <a href='https://dapper-stardust-7d2615.netlify.app/index.html' target="_blank" rel="noreferrer">Beatriz Barbosa</a> and is available on
+          <a href='https://github.com/beatrizmcbarbosa/weather-react' target="_blank" rel="noreferrer">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
