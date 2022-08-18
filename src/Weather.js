a83720d3d83c852cbbb85cb97b4338e80bfd25dc
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-18 10:33:51 +0100

    added loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,28 @@
 import React from "react";
+import axios from "axios";
+import { Audio } from 'react-loader-spinner';
 
-export default function Weather() {
+
+export default function Weather(props) {
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}ºC`);
+    }
+    let apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="Weather">
+            <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color='green'
+                ariaLabel='three-dots-loading'
+                wrapperStyle
+                wrapperClass
+            />
             <h2>Hello from Weather</h2>
         </div>
-    )
+    );
 }
