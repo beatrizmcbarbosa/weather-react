250c44295ac898636235426e5d54c4c5c21dcbc3
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-18 10:57:22 +0100

    Update Weather.js
    added weather app form

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css';
 import axios from "axios";
 import { Audio } from 'react-loader-spinner';
 
 
 export default function Weather(props) {
-    function handleResponse(response) {
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}ºC`);
+    let [city, setCity] = useState("");
+    let [weather, setWeather] = useState({});
+
+    function updateCity(event) {
+        setCity(event.target.value);
     }
-    let apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+
+    function showWeather(response) {
+        setWeather({
+            temperature: response.data.min.temp,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        });
+    }
+
+    function handleResponse(event) {
+        event.preventDefault();
+        let apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
 
     return (
         <div className="Weather">
@@ -22,7 +40,24 @@ export default function Weather(props) {
                 wrapperStyle
                 wrapperClass
             />
-            <h2>Hello from Weather</h2>
+            <h2>Weather App</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="search" placeholder="Enter a city" onChange={updateCity} />
+                <input type="submit" value="Search" />
+            </form>
+            <div>
+                <p>
+                    <ul>
+                        <li>Temperature: {weather.temperature}ºC</li>
+                        <li>Description: {weather.description}</li>
+                        <li>Humidity: {weather.humidity}%</li>
+                        <li>Wind: {weather.wind}kn/h</li>
+                        <li>
+                            <img src={weather.icon} alt={weather.description} />
+                        </li>
+                    </ul>
+                </p>
+            </div>
         </div>
     );
 }
