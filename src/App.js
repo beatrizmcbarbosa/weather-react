501b633e619c80506a346a00857c3c4fb566dc69
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-18 14:11:06 +0100

    apps.js github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <h2>Weather App</h2>
       <Weather />
+      <footer>
+        This project was created by <a href="https://dapper-stardust-7d2615.netlify.app/index.html" target="_blank">Beatriz Barbosa</a> and is {" "}
+        <a href="https://github.com/beatrizmcbarbosa/weather-react" target="_blank"></a>
+      </footer>
     </div>
   );
 }
