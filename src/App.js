14ca5665abd642ae8556698aa35c4ef77930d330
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-18 14:16:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <h2>Weather App</h2>
       <Weather />
       <footer>
-        This project was created by <a href='https://dapper-stardust-7d2615.netlify.app/index.html' target="_blank" rel="noreferrer">Beatriz Barbosa</a> and is {" "}
-        <a href='https://github.com/beatrizmcbarbosa/weather-react' target="_blank" rel="noreferrer"></a>
+        This project was created by <a href='https://dapper-stardust-7d2615.netlify.app/index.html' target="_blank" rel="noreferrer">Beatriz Barbosa</a> and is available on
+        <a href='https://github.com/beatrizmcbarbosa/weather-react' target="_blank" rel="noreferrer">GitHub</a>
       </footer>
     </div>
   );
