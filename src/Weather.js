65c67bc3799c97208c9926d5e45982c7d6a7ad3a
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-19 20:33:15 +0100

    removed precipitation error
    finally made this work!

--- src/Weather.js
+++ src/Weather.js
@@ -1,66 +1,73 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.city);
 
-    function handleResponse(response) {
-        setWeatherData({
-            ready: true,
-            temperature: response.data.main.temp,
-            date: new Date(response.data.dt * 1000),
-            description: response.data.weather[0].description,
-            precipitation: response.data.precipitation.value,
-            humidity: response.data.main.humidity,
-            wind: response.data.wind.speed,
-            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-        });
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
     function search() {
-        let apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);
+        const apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios
+            .get(apiUrl)
+            .then(response => {
+                const { main, dt, weather, wind, name } = response.data;
+
+                setWeatherData({
+                    ready: true,
+                    temperature: main.temp,
+                    date: new Date(dt * 1000),
+                    description: weather[0].description,
+                    humidity: main.humidity,
+                    wind: wind.speed,
+                    icon: weather[0].icon,
+                    city: name
+                });
+            })
+            .catch(error => {
+                console.log("Error fetching data, ", error);
+            });
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
     }
 
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
-                            <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" />
+                            <input
+                                type="search"
+                                placeholder="Enter a city"
+                                className="form-control"
+                                autoFocus="on"
+                                onChange={handleCityChange}
+                            />
                         </div>
                         <div className="col-3">
-                            <input type="submit" value="Search" className="btn btn-primary w-100"></input>
+                            <input
+                                type="submit"
+                                value="Search"
+                                className="btn btn-primary w-100"
+                            />
                         </div>
                     </div>
                 </form>
-                <h1>{weatherData.city}</h1>
-                <ul>
-                    <li><FormattedDate date={weatherData.date} /></li>
-                    <li>Description: {weatherData.description}</li>
-                </ul>
-                <div className="row">
-                    <div className="col-6">
-                        <img src={weatherData.icon} alt={weatherData.description} />
-                        <span className="temperature">{weatherData.temperature}</span>
-                        <span className="unit">ºC</span>
-                    </div>
-                    <div className="col-6">
-                        <ul>
-                            <li>Precipitation: {weatherData.precipitation}mm</li>
-                            <li>Humidity: {weatherData.humidity}%</li>
-                            <li>Wind: {weatherData.wind}kn/h</li>
-                        </ul>
-                    </div>
-                </div>
+                <WeatherInfo data={weatherData} />
             </div >
         );
     } else {
         search();
-        return "Loading";
+        return "Loading...";
     }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherInfo(props) {
+    return (<div className="WeatherInfo">
+        <h1>{props.data.city}</h1>
+        <ul>
+            <li><FormattedDate date={props.data.date} /></li>
+            <li>Description: {props.data.description}</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <div className="clearfix">
+                    <div className="float-left">
+                        <WeatherIcon code={props.data.icon} alt={props.data.description} />
+                    </div>
+                    <div className="float-left">
+                        <span className="temperature">{props.data.temperature}</span>
+                        <span className="unit">ºC</span>
+                    </div>
+                </div>
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Precipitation: {props.data.precipitation}mm</li>
+                    <li>Humidity: {props.data.humidity}%</li>
+                    <li>Wind: {props.data.wind}kn/h</li>
+                </ul>
+            </div>
+        </div>
+    </div>)
+
+}
