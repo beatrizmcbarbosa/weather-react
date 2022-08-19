06ef381b5f8642045a61630bf4c943b16b1fe86e
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-19 20:59:34 +0100

    edited weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,5 +1,32 @@
 import React from "react";
+import ReactAnimatedWeather from "react-animated-weather/build/ReactAnimatedWeather";
 
 export default function WeatherIcon(props) {
-    return <img src="https://raw.githubusercontent.com/divyanshu013/react-animated-weather/HEAD/react-animated-weather.gif" alt={props.alt.description} />
+    const codeMapping = {
+        "01d": "CLEAR_DAY",
+        "01n": "CLEAR_NIGHT",
+        "02d": "PARTYLY_CLOUDY_DAY",
+        "02n": "PARTYLY_CLOUDY_NIGHT",
+        "03d": "PARTYLY_CLOUDY_DAY",
+        "03n": "PARTYLY_CLOUDY_NIGHT",
+        "04d": "CLOUDY",
+        "04n": "CLOUDY",
+        "09d": "RAIN",
+        "09n": "RAIN",
+        "10d": "RAIN",
+        "10n": "RAIN",
+        "11d": "RAIN",
+        "11n": "RAIN",
+        "13d": "SNOW",
+        "13n": "SNOW",
+        "50d": "FOG",
+        "50n": "FOG"
+    }
+    return (
+        <ReactAnimatedWeather
+            icon={codeMapping[props.code]}
+            color="black"
+            size={50}
+            animate={true}
+        />);
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,17 +6,28 @@ export default function WeatherInfo(props) {
     return (<div className="WeatherInfo">
         <h1>{props.data.city}</h1>
         <ul>
-            <li><FormattedDate date={props.data.date} /></li>
-            <li>Description: {props.data.description}</li>
+            <li>
+                <FormattedDate date={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
         </ul>
         <div className="row">
-            <div className="col-6">
+            <div className="col-3">
                 <div className="clearfix">
                     <div className="float-left">
-                        <WeatherIcon code={props.data.icon} alt={props.data.description} />
+                        <WeatherIcon
+                            code={props.data.icon}
+                            alt={props.data.description}
+                        />
                     </div>
+                </div>
+            </div>
+            <div className="col-3">
+                <div className="clearfix">
                     <div className="float-left">
-                        <span className="temperature">{Math.round(props.data.temperature)}</span>
+                        <span className="temperature">
+                            {Math.round(props.data.temperature)}
+                        </span>
                         <span className="unit">ºC</span>
                     </div>
                 </div>
@@ -24,10 +35,10 @@ export default function WeatherInfo(props) {
             <div className="col-6">
                 <ul>
                     <li>Humidity: {props.data.humidity}%</li>
-                    <li>Wind: {props.data.wind}kn/h</li>
+                    <li>Wind: {props.data.wind}km/h</li>
                 </ul>
             </div>
-        </div>
-    </div>)
+        </div >
+    </div >)
 
 }
