37d8b0a81044fe39acc6c1d87d9bde140092056c
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-19 21:36:31 +0100

    created WeatherTemperature.js

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WeatherTemperature(props) {
+    return (
+        <div className="WeatherTemperature">
+            <span className="temperature">
+                {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit"> ºC</span>
+        </div>
+    );
+}
