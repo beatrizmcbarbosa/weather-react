ce22e418050e6fcb400ad2bd4c9f4cdfbfc61203
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-19 21:45:06 +0100

    added unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-    return (
-        <div className="WeatherTemperature">
-            <span className="temperature">
-                {Math.round(props.data.temperature)}
-            </span>
-            <span className="unit"> ºC</span>
-        </div>
-    );
+    const [unit, setUnit] = useState("celsius");
+
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    if (unit === 'celsius') {
+        return (
+            <div className="WeatherTemperature">
+                <span className="temperature">
+                    {Math.round(props.celsius)}
+                </span>
+                <span className="unit"> ºC |
+                    <a href="/" onClick={showFahrenheit}>
+                        ºF
+                    </a>
+                </span>
+            </div>
+        );
+    } else {
+        return (
+            <div className="WeatherTemperature">
+                <span className="temperature">
+                    {Math.round((props.celsius) * 9 / 5 + 32)}
+                </span>
+                <span className="unit">
+                    <a href="/" onClick={showCelsius}>
+                        ºC
+                    </a> | ºF
+                </span>
+            </div>
+        );
+    }
 }
