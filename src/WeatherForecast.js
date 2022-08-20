42b804902621c9986c3252455d2afa8f34e5e1ce
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-20 15:07:09 +0100

    Finalise errors

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -39,6 +39,8 @@ export default function WeatherForecast(props) {
                                     <WeatherForecastDay data={dailyForecast} />
                                 </div>
                             );
+                        } else {
+                            return null;
                         }
                     })}
                 </div>
