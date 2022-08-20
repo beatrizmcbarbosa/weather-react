0fa99c89e1b1edc8f79b3de3e1060c37ca89b259
beatrizmcbarbosa <biabarbosa98@gmail.com>
2022-08-20 15:03:46 +0100

    updated temperature

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 import "./WeatherForecast.css";
@@ -7,11 +7,17 @@ export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecastData, setForecastData] = useState(null);
 
+    // if coordinates change, set loaded to false - re-run
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+
     function search() {
         const apiKey = "7721f2269b88ab04c77bb88ab864eaa3";
         let lon = props.coordinates.lon;
         let lat = props.coordinates.lat;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&appid=${apiKey}&units=metric`; axios
+        let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+        axios
             .get(apiUrl)
             .then(response => {
                 setForecastData(response.data.daily);
@@ -26,9 +32,15 @@ export default function WeatherForecast(props) {
         return (
             <div className="WeatherForecast" >
                 <div className="row">
-                    <div className="col">
-                        <WeatherForecastDay data={forecastData[0]} />
-                    </div>
+                    {forecastData.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                            return (
+                                <div className="col" key={index}>
+                                    <WeatherForecastDay data={dailyForecast} />
+                                </div>
+                            );
+                        }
+                    })}
                 </div>
             </div>
         );
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -33,8 +33,10 @@ export default function WeatherForecastDay(props) {
             <div className="WeatherForecast-day">
                 {day()}
             </div>
-            <WeatherIcon code={props.data.weather[0].icon}
-                size={35} />
+            <WeatherIcon
+                code={props.data.weather[0].icon}
+                size={35}
+            />
             <div className="WeatherForecast-temperatures">
                 <span className="WeatherForecast-temperature-max">
                     {maxTemperature()}
